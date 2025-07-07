6bbac2698bae787f0d1a438c637b873d49f3dee3
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 11:30:21 -0300

    Troquei o throw da exceção por um println já que não tivemos aula sobre o tópico.

--- src/modelos/Celular.java
+++ src/modelos/Celular.java
@@ -45,7 +45,7 @@ public class Celular extends Produto {
      */
     public void setMemoriaRAM(int memoriaRAM) {
         if (memoriaRAM <= 0) {
-            throw new IllegalArgumentException("Memória RAM deve ser maior que 0");
+            System.out.println("Memoria RAM deve ser maior do que 0.")
         }
         this.memoriaRAM = memoriaRAM;
     }
