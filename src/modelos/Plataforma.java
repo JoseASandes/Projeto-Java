92cc698f50fc7942356f8bdbc979974babba6ce2
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 15:34:07 -0300

    Correções.

--- src/modelos/Plataforma.java
+++ src/modelos/Plataforma.java
@@ -1,22 +1,20 @@
 package modelos;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 import enums.StatusAvaliacao;
+import enums.StatusUsuario;
 import interfaces.Moderacao;
 
 public class Plataforma {
-    private List<Produto> produtos = new ArrayList<Produto>();
-    private List<Usuario> usuarios = new ArrayList<Usuario>();
+    private List<Produto> produtos = new ArrayList<>();
+    private List<Usuario> usuarios = new ArrayList<>();
 
     public void adicionarProduto(Produto p) {
         if (!produtos.contains(p)) {
             produtos.add(p);
             System.out.println("Produto " + p.getNome() + " adicionado com sucesso.");
-        }
-        else {
+        } else {
             System.out.println("O produto " + p.getNome() + " ja se encontra no sistema.");
         }
     }
@@ -25,38 +23,33 @@ public class Plataforma {
         if (!usuarios.contains(u)) {
             usuarios.add(u);
             System.out.println("Usuario " + u.getNome() + " cadastrado com sucesso.");
-        }
-        else {
+        } else {
             System.out.println("O usuario " + u.getNome() + " ja foi cadastrado no sistema.");
         }
     }
 
     public Produto buscarProdutoPorNome(String nome) {
-        return produtos.stream()
-                .filter(p -> p.nome.equalsIgnoreCase(nome))
-                .findFirst()
-                .orElse(null);
+        return produtos.stream().filter(p -> p.getNome().equalsIgnoreCase(nome)).findFirst().orElse(null);
     }
 
     public void listarProdutosOrdenadosPorNome() {
-        produtos.stream()
-                .sorted()
-                .forEach(p -> System.out.println(p.exibirDetalhes()));
+        produtos.stream().sorted().forEach(p -> System.out.println(p.exibirDetalhes()));
     }
 
     public List<Moderacao> listarPendenciasDeModeracao() {
         List<Moderacao> pendentes = new ArrayList<>();
-
         for (Usuario u : usuarios) {
-            if (u.getAvaliacoesFeitas().stream().anyMatch(a -> a.getStatus() == StatusAvaliacao.PENDENTE)) {
+            if (u.getStatus() == StatusUsuario.PENDENTE_APROVACAO) {
                 pendentes.add(u);
             }
         }
-
         for (Produto p : produtos) {
-            pendentes.addAll(p.getAvaliacoes().stream().filter(a -> a.getStatus() == StatusAvaliacao.PENDENTE).collect(Collectors.toList()));
+            for (Avaliacao a : p.getAvaliacoes()) {
+                if (a.getStatus() == StatusAvaliacao.PENDENTE) {
+                    pendentes.add(a);
+                }
+            }
         }
-
         return pendentes;
     }
 }
