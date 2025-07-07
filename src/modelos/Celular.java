d97a80483a7b98102b0dbe4a87d7d1c39b010659
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 15:36:21 -0300

    Correções.

--- src/modelos/Celular.java
+++ src/modelos/Celular.java
@@ -1,43 +1,41 @@
 package modelos;
 
-import java.util.List;
 public class Celular extends Produto {
     private int memoriaRAM;
     private int armazenamento;
 
-    public Celular() {
-    }
-
     public Celular(String nome, String marca, int memoriaRAM, int armazenamento) {
         super(nome, marca);
-        this.memoriaRAM = memoriaRAM;
-        this.armazenamento = armazenamento;
+        this.setMemoriaRAM(memoriaRAM);
+        this.setArmazenamento(armazenamento);
     }
-
-    public int getMemoriaRAM() {
-        return memoriaRAM;
-    }
-
+    
     public void setMemoriaRAM(int memoriaRAM) {
-        if (memoriaRAM <= 0) {
-            System.out.println("Memoria RAM deve ser maior do que 0.");
+        if (memoriaRAM > 0) {
+            this.memoriaRAM = memoriaRAM;
+        } else {
+            System.out.println("A memoria RAM deve ser maior do que 0.");
         }
-        this.memoriaRAM = memoriaRAM;
-    }
-
-    public int getArmazenamento() {
-        return armazenamento;
     }
 
     public void setArmazenamento(int armazenamento) {
-        if (armazenamento <= 0) {
-            System.out.println("Armazenamento deve ser maior que 0!");
+        if (armazenamento > 0) {
+            this.armazenamento = armazenamento;
+        } else {
+            System.out.println("O armazenamento deve ser maior que 0.");
         }
-        this.armazenamento = armazenamento;
     }
 
+    public int getMemoriaRAM() { 
+        return memoriaRAM; 
+    }
+
+    public int getArmazenamento() { 
+        return armazenamento; 
+    }
+    
     @Override
     public String exibirDetalhes() {
-        return String.format("Celular: %s %s, %d, %d", nome, marca, memoriaRAM, armazenamento);
+        return String.format("Celular: %s %s, RAM: %dGB, Armazenamento: %dGB", nome, marca, memoriaRAM, armazenamento);
     }
 }
