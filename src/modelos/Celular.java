22207cf86ddce9dfc15e59c49fbc35884b25eba3
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 12:16:27 -0300

    Limpeza do código.

--- src/modelos/Celular.java
+++ src/modelos/Celular.java
@@ -1,48 +1,23 @@
-/**
- * 
- */
 package modelos;
 
 import java.util.List;
-
-/**
- * 
- */
 public class Celular extends Produto {
     private int memoriaRAM;
     private int armazenamento;
 
-    /**
-     * 
-     */
-
     public Celular() {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param memoriaRAM
-     * @param armazenamento
-     * @param nome
-     * @param marca
-     * @param avaliacoes
-     */
     public Celular(String nome, String marca, int memoriaRAM, int armazenamento) {
         super(nome, marca);
         this.memoriaRAM = memoriaRAM;
         this.armazenamento = armazenamento;
     }
 
-    /**
-     * @return the memoriaRAM
-     */
     public int getMemoriaRAM() {
         return memoriaRAM;
     }
 
-    /**
-     * @param memoriaRAM the memoriaRAM to set
-     */
     public void setMemoriaRAM(int memoriaRAM) {
         if (memoriaRAM <= 0) {
             System.out.println("Memoria RAM deve ser maior do que 0.");
@@ -50,18 +25,10 @@ public class Celular extends Produto {
         this.memoriaRAM = memoriaRAM;
     }
 
-    /**
-     * @return the armazenamento
-     */
     public int getArmazenamento() {
         return armazenamento;
     }
 
-    /**
-     * @param armazenamento the armazenamento to set
-     *                      EVITA O ERRO DE VALORES COMO 0 OU MENOR QUE ZERO PARA
-     *                      MEMORIA
-     */
     public void setArmazenamento(int armazenamento) {
         if (armazenamento <= 0) {
             System.out.println("Armazenamento deve ser maior que 0!");
