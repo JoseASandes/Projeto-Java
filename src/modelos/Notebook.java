638332c7ba5de7000e8f5a4c46e15d36cc5a70e4
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 15:38:20 -0300

    Correções.

--- src/modelos/Notebook.java
+++ src/modelos/Notebook.java
@@ -1,40 +1,37 @@
 package modelos;
 
-import java.util.List;
 public class Notebook extends Produto {
     private String processador;
     private double tamanhoTela;
 
-    public Notebook() {
-    }
-
     public Notebook(String nome, String marca, String processador, double tamanhoTela) {
         super(nome, marca);
         this.processador = processador;
-        this.tamanhoTela = tamanhoTela;
-    }
-
-    public String getProcessador() {
-        return processador;
-    }
-
-    public void setProcessador(String processador) {
-        this.processador = processador;
-    }
-
-    public double getTamanhoTela() {
-        return tamanhoTela;
+        this.setTamanhoTela(tamanhoTela);
     }
 
     public void setTamanhoTela(double tamanhoTela) {
-        if (tamanhoTela <= 0) {
+        if (tamanhoTela > 0) {
+            this.tamanhoTela = tamanhoTela;
+        } else {
             System.out.println("O tamanho da tela deve ser maior que 0.");
         }
-        this.tamanhoTela = tamanhoTela;
+    }
+
+    public String getProcessador() { 
+        return processador; 
+    }
+
+    public void setProcessador(String processador) { 
+        this.processador = processador; 
+    }
+
+    public double getTamanhoTela() { 
+        return tamanhoTela; 
     }
 
     @Override
     public String exibirDetalhes() {
-        return String.format("Notebook: %s (%s), %s, %.1f", nome, marca, processador, tamanhoTela);
+        return String.format("Notebook: %s (%s), Processador: %s, Tela: %.1f\"", nome, marca, processador, tamanhoTela);
     }
 }
