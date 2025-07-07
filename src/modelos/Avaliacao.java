30b0f0d243a3c41e14c30b837e1c1b81c483f5bc
JoseASandes <joseantoniosandes20@gmail.com>
2025-07-07 15:26:56 -0300

    Correção.

--- src/modelos/Avaliacao.java
+++ src/modelos/Avaliacao.java
@@ -1,10 +1,11 @@
 package modelos;
 
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.*;
 
 import enums.StatusAvaliacao;
 import interfaces.Moderacao;
+
 public class Avaliacao implements Moderacao, Comparable<Avaliacao> {
     private int nota;
     private String texto;
@@ -14,7 +15,7 @@ public class Avaliacao implements Moderacao, Comparable<Avaliacao> {
     private Produto produtoAvaliado;
 
     public Avaliacao(int nota, String texto, Usuario autor, Produto produtoAvaliado) {
-        this.nota = nota;
+        this.setNota(nota);
         this.texto = texto;
         this.data = LocalDate.now();
         this.status = StatusAvaliacao.PENDENTE;
@@ -22,52 +23,32 @@ public class Avaliacao implements Moderacao, Comparable<Avaliacao> {
         this.produtoAvaliado = produtoAvaliado;
     }
 
-    public Avaliacao() {
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public Usuario getAutor() {
-        return autor;
-    }
-
-    public void setAutor(Usuario autor) {
-        this.autor = autor;
-    }
-
-    public Produto getProdutoAvaliado() {
-        return produtoAvaliado;
-    }
-
-    public void setProdutoAvaliado(Produto produtoAvaliado) {
-        this.produtoAvaliado = produtoAvaliado;
-    }
-
     public void setNota(int nota) {
-        if (nota <= 0 || nota > 5) {
-            System.out.println("Informe um valor entre 1 e 5.");
-        } else {
+        if (nota >= 1 && nota <= 5) {
             this.nota = nota;
+        } else {
+            System.out.println("A nota deve ser um valor entre 1 e 5.");
+
         }
     }
 
-    public void setStatus(StatusAvaliacao status) {
-        this.status = status;
+    public int getNota() {
+        return nota;
     }
+    
+    public StatusAvaliacao getStatus() {
+        return status;
+    }
+    
+    public String getTexto() { return texto; }
+    public void setTexto(String texto) { this.texto = texto; }
+    public LocalDate getData() { return data; }
+    public void setData(LocalDate data) { this.data = data; }
+    public Usuario getAutor() { return autor; }
+    public void setAutor(Usuario autor) { this.autor = autor; }
+    public Produto getProdutoAvaliado() { return produtoAvaliado; }
+    public void setProdutoAvaliado(Produto produtoAvaliado) { this.produtoAvaliado = produtoAvaliado; }
+    public void setStatus(StatusAvaliacao status) { this.status = status; }
 
     @Override
     public void aprovar() {
@@ -85,27 +66,13 @@ public class Avaliacao implements Moderacao, Comparable<Avaliacao> {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o)
+    public boolean equals(Object obj) {
+        if (this == obj)
             return true;
-        if (!(o instanceof Avaliacao))
+        if (!(obj instanceof Avaliacao))
             return false;
-        Avaliacao a = (Avaliacao) o;
-        return nota == a.nota && Objects.equals(texto, a.texto)
-                && Objects.equals(autor, a.autor)
+        Avaliacao a = (Avaliacao) obj;
+        return Objects.equals(autor, a.autor)
                 && Objects.equals(produtoAvaliado, a.produtoAvaliado);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nota, texto, autor, produtoAvaliado);
-    }
-
-    public int getNota() {
-        return nota;
-    }
-
-    public StatusAvaliacao getStatus() {
-        return status;
     }
 }
